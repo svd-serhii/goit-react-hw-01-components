bf4e38d7d935556ae9abfe141e01e27c7f6d9443
Serhii Demchenko <svdemchenko@gmail.com>
2023-01-12 19:09:24 +0200

    add stat component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -29,7 +29,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   );
 };
 
-Profile.prototype = {
+Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,26 @@
 import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
-export const Statistics = () => {
+console.log(css);
+
+export const Statistics = ({ title, stats }) => {
   return (
-    <section class="statistics">
-      <h2 class="title">Upload stats</h2>
-
-      <ul class="stat-list">
-        <li class="item">
-          <span class="label">.docx</span>
-          <span class="percentage">4%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li>
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li key={stat.id} className={`${css.item} ${css[stat.id]}`}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
+};
+
+Statistics.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.string.isRequired,
 };
